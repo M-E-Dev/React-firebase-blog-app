7b802ec7b418a585baef31df8686e6e740f21a18
Muhammed Eroğlu <llofmejb@gmail.com>
2022-07-31 18:55:32 +0300

    2

--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -2,14 +2,15 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import About from "../pages/About";
 import Dashboard from "../pages/Dashboard";
 import Details from "../pages/Details";
-import Login from "../pages/Login";
+// import Login from "../pages/Login";
 import NewBlog from "../pages/NewBlog";
 import Profile from "../pages/Profile";
-import Register from "../pages/Register";
+// import Register from "../pages/Register";
 import UpdateBlog from "../pages/UpdateBlog";
 import NotFound from "../pages/NotFound";
 import PrivateRouter from "./PrivateRouter";
 import Navbar from "../components/Navbar";
+import Authorization from "../components/Authorization";
 
 const AppRouter = () => {
   return (
@@ -25,8 +26,7 @@ const AppRouter = () => {
 
         <Route path="/" exact element={<Dashboard />} />
         <Route path="/about" element={<About />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
+        <Route path="/authorization" element={<Authorization />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>

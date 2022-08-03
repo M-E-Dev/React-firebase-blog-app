09b8def746ffd34fa32bf8d7a8fbe61c991e9fb2
Muhammed Eroğlu <llofmejb@gmail.com>
2022-08-03 18:25:00 +0300

    5

--- src/utils/firebaseUtil.js
+++ src/utils/firebaseUtil.js
@@ -3,13 +3,14 @@ import "firebase/compat/auth";
 import "firebase/compat/database";
 
 const firebaseUtil = firebase.initializeApp({
-  apiKey:"AIzaSyChlCC7DciNebj3lccCKg8Lt3MziRF1wUI",
-  authDomain:"fir-blog-app-3184e.firebaseapp.com",
-  projectId:"fir-blog-app-3184e",
-  storageBucket:"fir-blog-app-3184e.appspot.com",
-  messagingSenderId:"903139893681",
-  appId:"1:903139893681:web:08953024e331478f345d90",
-  databaseURL:"https://crud-reactjs-69323-default-rtdb.firebaseio.com/"
+  apiKey: "AIzaSyChlCC7DciNebj3lccCKg8Lt3MziRF1wUI",
+  authDomain: "fir-blog-app-3184e.firebaseapp.com",
+  databaseURL: "https://fir-blog-app-3184e-default-rtdb.firebaseio.com",
+  projectId: "fir-blog-app-3184e",
+  storageBucket: "fir-blog-app-3184e.appspot.com",
+  messagingSenderId: "903139893681",
+  appId: "1:903139893681:web:08953024e331478f345d90"
+
 });
 // const firebaseUtil = firebase.initializeApp({
 //   apiKey:process.env.REACT_APP_FIREBASE_apiKey,

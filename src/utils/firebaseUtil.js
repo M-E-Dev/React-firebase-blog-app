7905ba58aae6ea3013e68846144fff82197a0ec8
Muhammed Eroğlu <llofmejb@gmail.com>
2022-08-03 17:49:31 +0300

    3

--- src/utils/firebaseUtil.js
+++ src/utils/firebaseUtil.js
@@ -3,14 +3,23 @@ import "firebase/compat/auth";
 import "firebase/compat/database";
 
 const firebaseUtil = firebase.initializeApp({
-  apiKey:process.env.REACT_APP_FIREBASE_apiKey,
-  authDomain:process.env.REACT_APP_FIREBASE_authDomain,
-  projectId:process.env.REACT_APP_FIREBASE_projectId,
-  storageBucket:process.env.REACT_APP_FIREBASE_storageBucket,
-  messagingSenderId:process.env.REACT_APP_FIREBASE_messagingSenderId,
-  appId:process.env.REACT_APP_FIREBASE_appId,
-  databaseURL: process.env.REACT_APP_databaseURL
+  apiKey:"AIzaSyChlCC7DciNebj3lccCKg8Lt3MziRF1wUI",
+  authDomain:"fir-blog-app-3184e.firebaseapp.com",
+  projectId:"fir-blog-app-3184e",
+  storageBucket:"fir-blog-app-3184e.appspot.com",
+  messagingSenderId:"903139893681",
+  appId:"1:903139893681:web:08953024e331478f345d90",
+  databaseURL:"https://crud-reactjs-69323-default-rtdb.firebaseio.com/"
 });
+// const firebaseUtil = firebase.initializeApp({
+//   apiKey:process.env.REACT_APP_FIREBASE_apiKey,
+//   authDomain:process.env.REACT_APP_FIREBASE_authDomain,
+//   projectId:process.env.REACT_APP_FIREBASE_projectId,
+//   storageBucket:process.env.REACT_APP_FIREBASE_storageBucket,
+//   messagingSenderId:process.env.REACT_APP_FIREBASE_messagingSenderId,
+//   appId:process.env.REACT_APP_FIREBASE_appId,
+//   databaseURL: process.env.REACT_APP_databaseURL
+// });
 export default firebaseUtil;
 
 export const auth = firebaseUtil.auth();
